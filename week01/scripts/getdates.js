a77c5be4eb4cd718e586941671e0e563db0d93d4
tecklermakomeke-max <tecklermakomeke@gmail.com>
2026-07-01 10:43:06 +0200

    test

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -1,5 +1,5 @@
 const yearSpan = document.querySelector("#currentyear");
 const today = new Date();
 yearSpan.innerHTML = today.getFullYear();
-const lastMod = document.querySelector("lastModified");
-lastMod.textContent = 'Last Modification: ${document.lastModified}';
+const lastMod = document.querySelector("#lastModified");
+lastMod.textContent = `Last Modification: ${document.lastModified}`;
