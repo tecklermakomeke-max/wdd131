38650ca301abb3ea1d69def1ca36cf47f6e00f83
tecklermakomeke-max <tecklermakomeke@gmail.com>
2026-07-09 22:45:29 +0200

    test

---
+++ scripts/temples.js
@@ -0,0 +1,11 @@
+// footer: year + Last Modified
+const yearSpan = document.querySelector("#year");
+const lastModified = document.querySelector("#lastModified");
+yearSpan.textContent = new Date().getFullYear();
+lastMod.textContent = 'Last Modification: ${document.lastModified}';
+const menuButton = document.querySelector('#menu');
+const nav= document.querySelector('nav');
+menuButton.addEventListener('click', () =>{
+    nav.classList.toggle('open');
+    menuButton.textContent = nav.classList.contains('open') ? 'X' : '=';
+})
