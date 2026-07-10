44a0a80b129439a2ddc11e8940ef56a87de8b171
tecklermakomeke-max <tecklermakomeke@gmail.com>
2026-07-10 13:56:59 +0200

    test

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,5 +7,5 @@ const menuButton = document.querySelector('#menu');
 const nav= document.querySelector('nav');
 menuButton.addEventListener('click', () =>{
     nav.classList.toggle('open');
-    menuButton.textContent = nav.classList.contains('open') ? 'X' : '=';
+    menuButton.textContent = nav.classList.contains('open') ? 'X' : 'hamuburger';
 })
