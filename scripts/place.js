4eb194c1fdbef0e24f1f42708702fcafc35ee732
tecklermakomeke-max <tecklermakomeke@gmail.com>
2026-07-17 21:08:44 +0200

    test

--- scripts/place.js
+++ scripts/place.js
@@ -1,6 +1,6 @@
 // set up current year and last modified date
-document.getElementByld("year").textContent = new Date().getFullYear();
-document.getElementByld("lastModified").textContent = document.lastModified;
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
 // Paris weather - static values
 const temp = 8; // ℃ - Paris winter day
 const windSpeed = 15; // km/h - windy and breeze
@@ -8,7 +8,7 @@ const windSpeed = 15; // km/h - windy and breeze
 function calculateWindChill(temperature, wind) {
     return 13.12 + 0.6215 * temperature - 11.37 * Math.pow(wind, 0.16) + 0.3965 * temperature * Math.pow(wind, 0.16);
 } 
-let windChillValue = document.getElementById('windchill');
+let windChillElement = document.getElementById('windchill');
 if (temp <= 10 && windSpeed > 4.8) {
     let windChill = calculateWindChill(temperature, windSpeed);
         windChillElement.textContent = '${windChill.toFixed(1)} ℃';
