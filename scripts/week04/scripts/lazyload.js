591d17d892c4f4493224f48629c33e4c9dbfc95f
tecklermakomeke-max <tecklermakomeke@gmail.com>
2026-07-21 13:27:56 +0200

    test

---
+++ scripts/week04/scripts/lazyload.js
@@ -0,0 +1,3 @@
+// set current year and last modification date
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified
